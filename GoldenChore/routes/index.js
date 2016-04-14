93f3839d1a1c2d69fcadc9a2dea19e66e3c731b0
Tao Wu <taowut@umich.edu>
2016-04-14 14:38:55 -0400

    integrate 3 sites

--- GoldenChore/routes/index.js
+++ GoldenChore/routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('home');
+});
+
 /* GET index page. */
 router.get('/', function(req, res, next) {
   res.render('index', {expressFlash: req.flash('message') });
